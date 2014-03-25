42a8e82930fc40e20a27f5014702f8d234255ac1
Shepherd Kendall <skendall@gmail.com>
2014-03-25 14:34:13 -0400

    changing direction

--- src/main/scala/org/kndl/sock/Model.scala
+++ src/main/scala/org/kndl/sock/Model.scala
@@ -1,5 +1,20 @@
 package org.kndl.sock
 
-class V(val id: Long, val name: String)
-class E(val id: Long, val vA: V, val vB: V, val w: Double)
-class G(val id: Long, val e: Seq[E])
+import scala.slick.direct.AnnotationMapper.{column, table}
+
+@table("V")class V(
+   @column("id")val id: Long,
+   @column("name")val name: String
+)
+
+@table("E")class E(
+   @column("id")val id: Long,
+   @column("vA")val vA: V,
+   @column("vB")val vB: V,
+   @column("w")val w: Double
+)
+
+@table("G")class G(
+   @column("id")val id: Long,
+   @column("e")val e: Seq[E]
+)
--- src/main/scala/org/kndl/sock/ds/DataStore.scala
+++ src/main/scala/org/kndl/sock/ds/DataStore.scala
@@ -1,14 +1,20 @@
 package org.kndl.sock.ds
 
 import java.util.concurrent.atomic.AtomicInteger
-
+import scala.slick.driver.H2Driver.simple._
+import scala.slick.direct.{SlickBackend, AnnotationMapper}
+import scala.slick.driver.H2Driver
 
 object DataStore {
 
   val id: AtomicInteger = new AtomicInteger(1)
 
-  Database.forURL
+  val backend = new SlickBackend(H2Driver, AnnotationMapper)
 
-  val map = PersistentMap.create[String,String]("graph", database)
+  def createVertex(name: String) {
+    Database.forURL("jdbc:h2:graph", driver = "org.h2.Driver") withSession { implicit session:Session =>
+
+    }
+  }
 
 }
