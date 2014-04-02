17bc30c76c4bd855a5e5845805d11738d9e9d933
Shepherd Kendall <skendall@gmail.com>
2014-04-02 15:00:25 -0400

    started dijkstra implementation

--- src/main/scala/org/kndl/sock/algo/DijkstraSPF.scala
+++ src/main/scala/org/kndl/sock/algo/DijkstraSPF.scala
@@ -1,7 +1,28 @@
 package org.kndl.sock.algo
 
-import org.kndl.sock.G
+import org.kndl.sock.{V, G}
 
-class DijkstraSPF extends GraphAlgorithm {
-  def calculate(graph: G): G = ???
+class DijkstraSPF {
+
+  private class Node(vertex: V, visited: Boolean)
+
+  def calculateSPF(graph: G, start: V, end: V): G = {
+    var nodes: Map[V,Double] = Map()
+    for( v <- graph.vertices) {
+      if(v.name == start.name) {
+        nodes ++= Map(v -> 0)
+      } else {
+        nodes ++= Map(v -> -1)
+      }
+      v.set
+    }
+
+    var current = start
+
+    neighbors(graph,current)
+
+    graph
+  }
+
+  def neighbors(graph: G, vertex: V): Seq[V] = graph.edges(vertex).filter { edge => edge.vA == vertex }.map { edge => edge.vB }
 }
