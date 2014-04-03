264de751aa5f5dd2eebfbd8dcc5554354ab66496
Shepherd Kendall <skendall@gmail.com>
2014-04-03 14:02:13 -0400

    finally finished set conversion

--- src/main/scala/org/kndl/sock/V.scala
+++ src/main/scala/org/kndl/sock/V.scala
@@ -6,8 +6,8 @@ case class V(val name: String) extends scala.Serializable {
     E(this,v,w)
   }
 
-  def <->(v: V, weightTo: Double, weightFrom: Double): Seq[E] = {
-    Seq(new E(this,v,weightTo), new E(v,this,weightFrom))
+  def <->(v: V, weightTo: Double, weightFrom: Double): Set[E] = {
+    Set(new E(this,v,weightTo), new E(v,this,weightFrom))
   }
 
   override def equals(other: Any) = other.isInstanceOf[V] && other.asInstanceOf[V].name == name
