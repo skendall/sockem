c1332db1456dac7aed6915bd642a3e5ef3a5694a
Shepherd Kendall <skendall@gmail.com>
2014-04-03 13:46:34 -0400

    added asserts

--- src/test/scala/org/kndl/sock/algo/test/DijkstraTest.scala
+++ src/test/scala/org/kndl/sock/algo/test/DijkstraTest.scala
@@ -1,7 +1,7 @@
 package org.kndl.sock.algo.test
 
 import org.scalatest.{Matchers, FlatSpec}
-import org.kndl.sock.{V, G}
+import org.kndl.sock.{E, V, G}
 import org.kndl.sock.aglo.spf.Dijkstra
 
 class DijkstraTest extends FlatSpec with Matchers {
@@ -13,12 +13,13 @@ class DijkstraTest extends FlatSpec with Matchers {
     val from = "v1"
     val to = "v4"
 
-    graph ++ V("v1") ++ V("v2") ++ V("v3") ++ V("v4") +| ("v1","v2",5) +| ("v1","v3",5) +| ("v2","v4",2) +| ("v3","v4",5)
+    graph ++ "v1" ++ "v2" ++ "v3" ++ "v4" +| ("v1","v2",5) +| ("v1","v3",5) +| ("v2","v4",2) +| ("v3","v4",5)
 
     val (distance,path) = Dijkstra(graph,from,to)
 
-    println(s"The shortest path from $from to $to has:")
-    println(s" - length: $distance")
-    println(s" - nodes: " + path.mkString("[ ", " -> ", " ]"))
+    assert(path(0) == "v1")
+    assert(path(1) == "v2")
+    assert(path(2) == "v4")
   }
+
 }
