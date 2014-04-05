cb5466a2a3321f1ee0c1a366d1766dc3bcaabc29
Shepherd Kendall <skendall@gmail.com>
2014-04-05 13:55:05 -0400

    removed unused objects

--- src/main/scala/org/kndl/sock/aglo/spf/AStar.scala
+++
@@ -1,8 +0,0 @@
-package org.kndl.sock.aglo.spf
-
-import org.kndl.sock.G
-import org.kndl.sock.algo.GraphAlgorithm
-
-class AStar extends GraphAlgorithm {
-  def calculate(graph: G): G = ???
-}
--- src/main/scala/org/kndl/sock/algo/GraphAlgorithm.scala
+++
@@ -1,7 +0,0 @@
-package org.kndl.sock.algo
-
-import org.kndl.sock.G
-
-trait GraphAlgorithm {
-  def calculate(graph: G): G
-}
