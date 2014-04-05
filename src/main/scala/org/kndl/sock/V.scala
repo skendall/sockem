5e38c389ee99331a33bf2f4818fdfe5c59b53b74
Shepherd Kendall <skendall@gmail.com>
2014-04-05 13:21:15 -0400

    added vertex enabling capability

--- src/main/scala/org/kndl/sock/V.scala
+++ src/main/scala/org/kndl/sock/V.scala
@@ -6,12 +6,26 @@ case class V(val name: String) extends scala.Serializable {
 
   private var metaData: Seq[VertexMetaData] = Seq()
 
+  private var enabled: Boolean = true
+
   def set(key: String, value: Any):Unit = {
     metaData = metaData :+ VertexMetaData(key,value,System.currentTimeMillis())
   }
 
   def get(key: String):Option[VertexMetaData] = {
     metaData.find { vmd => vmd.key == key }
+  }
+
+  def isEnabled:Boolean = enabled
+
+  def enable:V = {
+    enabled = true
+    this
+  }
+
+  def disable:V =  {
+    enabled = false
+    this
   }
 
   def ->(v: V,w: Double): E = {
