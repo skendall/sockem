4dd73d6955b46d03c6c7a8f1870dff8b4693e988
Shepherd Kendall <skendall@gmail.com>
2014-04-07 08:05:45 -0400

    fixed test, removed unnecessary overrides

--- build.sbt
+++ build.sbt
@@ -9,5 +9,5 @@ scalaVersion := "2.10.3"
 publishTo := Some(Resolver.file("file",  new File( "maven2" )) )
 
 libraryDependencies ++= Seq(
-    "org.scalatest" %% "scalatest" % "2.1.0" % "test",
+    "org.scalatest" %% "scalatest" % "2.1.0" % "test"
 )
--- src/main/scala/org/kndl/sockem/V.scala
+++ src/main/scala/org/kndl/sockem/V.scala
@@ -33,10 +33,5 @@ case class V(val name: String) extends scala.Serializable {
   def <->(v: V, weightTo: Double, weightFrom: Double): Set[E] = {
     Set(new E(this,v,weightTo), new E(v,this,weightFrom))
   }
-
-  override def equals(other: Any) = other.isInstanceOf[V] && other.asInstanceOf[V].name == name
-
-  override def toString = name
-
 }
 
